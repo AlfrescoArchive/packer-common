847bf8bbda569dbeb62c0ecbfc3d66ba71b5bbea
Maurizio Pillitu <maoo@apache.org>
2015-04-14 15:43:18 +0200

    Update packer-builder-amazon.rb
    added chef_log_level

--- packer-builder-amazon.rb
+++ packer-builder-amazon.rb
@@ -17,7 +17,8 @@ Racker::Processor.register_template do |t|
     "ssh_username" => "{{env `SSH_USERNAME`}}",
     # Chef Provisioning parameters
     "data_bags_path" => "{{env `DATA_BAGS_PATH`}}",
-    "cookbook_path" => "{{env `COOKBOOK_PATH`}}"
+    "cookbook_path" => "{{env `COOKBOOK_PATH`}}",
+    "chef_log_level" => "{{env `CHEF_LOG_LEVEL`}}"
   }
 
   t.builders['amazon'] = {
