092f455ea5c440a387ce5becff1a55b65d4e2e3b
Maurizio Pillitu <maoo@apache.org>
2015-04-29 18:59:16 +0200

    Update chef-zero-client.rb

--- chef-zero-client.rb
+++ chef-zero-client.rb
@@ -8,19 +8,4 @@ data_bag_path = "/etc/chef/databags"
 # Default Packer template
 log_level        :info
 log_location     STDOUT
-chef_server_url  "{{.ServerUrl}}"
-{{if ne .ValidationClientName ""}}
-validation_client_name "{{.ValidationClientName}}"
-{{else}}
-validation_client_name "chef-validator"
-{{end}}
-{{if ne .ValidationKeyPath ""}}
-validation_key "{{.ValidationKeyPath}}"
-{{end}}
 node_name "{{.NodeName}}"
-{{if ne .ChefEnvironment ""}}
-environment "{{.ChefEnvironment}}"
-{{end}}
-{{if ne .SslVerifyMode ""}}
-ssl_verify_mode :{{.SslVerifyMode}}
-{{end}}
