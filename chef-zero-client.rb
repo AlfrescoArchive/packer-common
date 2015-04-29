6f3b25a3cae6c67c1c433fb358d37a94eee137a7
Maurizio Pillitu <maoo@apache.org>
2015-04-29 18:48:59 +0200

    Update chef-zero-client.rb

--- chef-zero-client.rb
+++ chef-zero-client.rb
@@ -1,9 +1,9 @@
 #static values added by packer-common to enable chef-zero
 local_mode = true
-chef_repo_path = /etc/chef
+chef_repo_path = "/etc/chef"
 
-cookbook_path = /etc/chef/cookbooks
-data_bag_path = /etc/chef/databags
+cookbook_path = "/etc/chef/cookbooks"
+data_bag_path = "/etc/chef/databags"
 
 # Default Packer template
 log_level        :info
