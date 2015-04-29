f038d5a4ca20272a2899b054f2344a15b8bbdad5
Maurizio Pillitu <maoo@apache.org>
2015-04-29 16:41:30 +0200

    Update packer-provisioner-chef-zero.rb

--- packer-provisioner-chef-zero.rb
+++ packer-provisioner-chef-zero.rb
@@ -1,9 +1,14 @@
 Racker::Processor.register_template do |t|
   t.provisioners = {
+    9 => {
+      "type" => "shell",
+      "inline" => [
+        "sudo mkdir -p /etc/chef && sudo chown -R root:root /etc/chef",
+        "sudo mkdir -p /tmp/packer-chef-client && sudo chown -R root:root /tmp/packer-chef-client"
+      ]
+    },
     10 => {
       "chef" => {
-        # "cookbook_paths"=> ["{{user `cookbook_path`}}"],
-        # "data_bags_path" => "{{user `data_bags_path`}}",
         "execute_command"=> "sudo chef-client --no-color --local-mode -c {{.ConfigPath}} -j {{.JsonPath}} -l {{user `chef_log_level`}}",
         "server_url" => "http://localhost:8889",
         "install_command"=> "sudo bash -c 'curl -L https://www.opscode.com/chef/install.sh| bash -s -- -v 12.2.1'",
