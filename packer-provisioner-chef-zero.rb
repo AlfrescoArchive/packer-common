739bef0b40ae279533f1dd30e645e0b4d020ce29
Maurizio Pillitu <maoo@apache.org>
2015-04-30 12:35:54 +0200

    Update packer-provisioner-chef-zero.rb

--- packer-provisioner-chef-zero.rb
+++ packer-provisioner-chef-zero.rb
@@ -4,16 +4,8 @@ Racker::Processor.register_template do |t|
       "create-packer-folders" => {
         "type" => "shell",
         "inline" => [
-          "set -e",
-          "curl -L https://www.opscode.com/chef/install.sh | sudo bash -s -- -v 12.2.1",
-          "install -d -m 755 -o ubuntu /tmp/packer-chef-client",
-          "sudo install -d -m 777 -o root -g root /etc/chef"
-          # "sudo mkdir -p /etc/chef",
-          # "sudo chmod 777 /etc/chef",
-          # "sudo chown -R root:root /etc/chef",
-          # "sudo mkdir -p /tmp/packer-chef-client",
-          # "sudo chown -R root:root /tmp/packer-chef-client",
-          # "sudo chmod 777 /tmp/packer-chef-client"
+          "sudo mkdir -p /etc/chef",
+          "sudo chmod 777 /etc/chef",
         ]
       }
     },
@@ -31,55 +23,19 @@ Racker::Processor.register_template do |t|
         "destination"=> "/etc/chef/data_bags"
       }
     },
-    8 => {
-      "copy-chef-pem" => {
-        "type" => "file",
-        "source"=> "{{pwd}}/packer_common_checkout/chef.pem",
-        "destination"=> "/etc/chef/chef.pem"
-      }
-    },
-    9 => {
-      "copy-knife-rb" => {
-        "type" => "file",
-        "source"=> "{{pwd}}/packer_common_checkout/knife.rb",
-        "destination"=> "/etc/chef/knife.rb"
-      }
-    },
-    # 10 => {
-    #   "chef" => {
-    #     "execute_command"=> "cd /etc/chef && \
-    #     sudo chef-client --no-color --local-mode \
-    #     -c /tmp/packer-chef-client/client.rb \
-    #     -j /tmp/packer-chef-client/first-boot.json \
-    #     -l {{user `chef_log_level`}} && \
-    #     sudo knife node delete localhost -c /etc/chef/knife.rb -z -k /etc/chef/chef.pem -y && \
-    #     sudo knife client delete localhost -c /etc/chef/knife.rb -z -k /etc/chef/chef.pem -y",
-    #     "server_url" => "http://localhost:8889",
-    #     "install_command"=> "sudo bash -c 'curl -L https://www.opscode.com/chef/install.sh| bash -s -- -v 12.2.1'",
-    #     "prevent_sudo"=> false,
-    #     "skip_install"=> false,
-    #     "type"=> "chef-client",
-    #     "config_template" => "{{pwd}}/packer_common_checkout/chef-zero-client.rb"
-    #   }
-    # },
     10 => {
-      "run-chef-zero" => {
-        "type" => "shell",
-        "inline"=> [
-          "cd /etc/chef",
-          "sudo chef-client --no-color --local-mode \
-          -c /tmp/packer-chef-client/client.rb \
-          -j /tmp/packer-chef-client/first-boot.json \
-          -l :info",
-          "sudo knife node delete localhost -c /etc/chef/knife.rb -z -k /etc/chef/chef.pem -y",
-          "sudo knife client delete localhost -c /etc/chef/knife.rb -z -k /etc/chef/chef.pem -y"
-        ]
-        # "server_url" => "http://localhost:8889",
-        # "install_command"=> "sudo bash -c 'curl -L https://www.opscode.com/chef/install.sh| bash -s -- -v 12.2.1'",
-        # "prevent_sudo"=> false,
-        # "skip_install"=> false,
-        # "type"=> "chef-client",
-        # "config_template" => "{{pwd}}/packer_common_checkout/chef-zero-client.rb"
+      "chef" => {
+        "cookbook_paths"=> ["{{user `cookbook_path`}}"],
+        "data_bags_path" => "{{user `data_bags_path`}}",
+          "execute_command"=> "cd /etc/chef && sudo chef-client --no-color --local-mode \
+          -c {{.ConfigPath}} \
+          -j {{.JsonPath}} \
+          -l {{user `chef_log_level`}}",
+        "install_command"=> "sudo bash -c 'curl -L https://www.opscode.com/chef/install.sh| bash -s -- -v 12.2.1'",
+        "prevent_sudo"=> false,
+        "skip_install"=> false,
+        "type"=> "chef-solo",
+        "config_template" => "{{pwd}}/packer_common_checkout/chef-zero-client.rb"
       }
     },
     100 => {
