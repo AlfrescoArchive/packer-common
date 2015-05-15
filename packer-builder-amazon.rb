a7655afc3ae9e101ff96ccc93126d82544874fbc
Maurizio Pillitu <maoo@apache.org>
2015-05-15 11:23:11 +0200

    Update packer-builder-amazon.rb

--- packer-builder-amazon.rb
+++ packer-builder-amazon.rb
@@ -16,6 +16,7 @@ Racker::Processor.register_template do |t|
     # Coordinates for generated AMI
     "ami_name"=> "{{env `AMI_NAME`}}",
     "ami_description"=> "{{env `AMI_DESCRIPTION`}}",
+    "ami_user_1" => "{{env `AMI_USER_1`}}",
     "ssh_username" => "{{env `SSH_USERNAME`}}",
     # Chef Provisioning parameters
     "data_bags_path" => "{{env `DATA_BAGS_PATH`}}",
@@ -37,6 +38,7 @@ Racker::Processor.register_template do |t|
     "ami_name"=> "{{user `ami_name`}}",
     "ami_description"=> "{{user `ami_description`}}",
     "ami_virtualization_type" => "{{user `ami_virtualization_type`}}",
+    "ami_users" => ["{{user `ami_user_1`}}"],
     "enhanced_networking" => "{{user `enhanced_networking`}}",
   }
 end
