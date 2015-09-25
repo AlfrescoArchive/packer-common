44e3e55651d1dd9148430ee722f812f25b217ed0
Maurizio Pillitu <maoo@apache.org>
2015-09-26 00:35:57 +0200

    Update run-release.sh

--- run-release.sh
+++ run-release.sh
@@ -16,11 +16,11 @@ set -e
 # Right now it only supports HTTP Git urls
 if [ -z "$ARTIFACT_ID" ]; then
   export ARTIFACT_ID=`echo ${GIT_REPO%????} | cut -d "/" -f 5`
-  echo "[release.sh] Setting ARTIFACT_ID=$ARTIFACT_ID"
+  echo "[run-release.sh] Setting ARTIFACT_ID=$ARTIFACT_ID"
 fi
 if [ -z "$GIT_PROJECT_NAME" ]; then
   export GIT_PROJECT_NAME=$ARTIFACT_ID
-  echo "[release.sh] Setting GIT_PROJECT_NAME=$ARTIFACT_ID"
+  echo "[run-release.sh] Setting GIT_PROJECT_NAME=$ARTIFACT_ID"
 fi
 
 export GIT_PREFIX=git@github.com
@@ -33,7 +33,7 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 function buildArtifact () {
-  echo "[release.sh] Building Chef artifact"
+  echo "[run-release.sh] Building Chef artifact"
   /opt/chefdk/embedded/bin/rake
 }
 
@@ -51,7 +51,7 @@ function incrementVersion () {
   export currentVersion=$(getCurrentVersion)
   export nextVersion=$(getIncrementedVersion)
 
-  echo "[release.sh] Incrementing version from $currentVersion to $nextVersion"
+  echo "[run-release.sh] Incrementing version from $currentVersion to $nextVersion"
 
   sed "s/$currentVersion/$nextVersion/" metadata.rb > metadata.rb.tmp
   rm -f metadata.rb
@@ -59,7 +59,7 @@ function incrementVersion () {
 }
 
 function deploy () {
-  echo "[release.sh] Deploy $1"
+  echo "[run-release.sh] Deploy $1"
   repo_name="internal-releases"
   if [[ $1 == *SNAPSHOT ]]; then
     repo_name="internal-snapshots"
@@ -69,14 +69,14 @@ function deploy () {
 }
 
 function deploySnapshot () {
-  echo "[release.sh] deploy snapshot disabled"
+  echo "[run-release.sh] deploy snapshot disabled"
   buildArtifact
   current_version=$(getCurrentVersion)
   deploy "$current_version-SNAPSHOT"
 }
 
 function release () {
-  echo "[release.sh] invoking Packer"
+  echo "[run-release.sh] invoking Packer"
   
   export VERSION=$(getCurrentVersion)
   
@@ -84,24 +84,24 @@ function release () {
     curl -L https://raw.githubusercontent.com/Alfresco/packer-common/master/run-packer.sh --no-sessionid | bash -s -- ./ami.env
   fi
   
-  echo "[release.sh] Packer completed"
-  echo "[release.sh] deploy release disabled"
-  echo "[release.sh] Setting git remote to $GIT_PREFIX:$GIT_ACCOUNT_NAME/$GIT_PROJECT_NAME.git"
+  echo "[run-release.sh] Packer completed"
+  echo "[run-release.sh] deploy release disabled"
+  echo "[run-release.sh] Setting git remote to $GIT_PREFIX:$GIT_ACCOUNT_NAME/$GIT_PROJECT_NAME.git"
   git remote set-url origin $GIT_PREFIX:$GIT_ACCOUNT_NAME/$GIT_PROJECT_NAME.git
-  echo "[release.sh] Git tag $(getCurrentVersion)"
+  echo "[run-release.sh] Git tag $(getCurrentVersion)"
   git tag -a "v$(getCurrentVersion)" -m "releasing v$(getCurrentVersion)"
   git push origin --tags
   buildArtifact
   deploy $(getCurrentVersion)
   incrementVersion
-  echo "[release.sh] Git stash/pull/pop/commit/push"
+  echo "[run-release.sh] Git stash/pull/pop/commit/push"
   git stash
   git pull origin master
   git stash pop
   git add metadata.rb
   git commit -m "Bumping version to v$next_version"
   git push origin master
-  echo "[release.sh] Release completed!"
+  echo "[run-release.sh] Release completed!"
 }
 
 MODE=$1
