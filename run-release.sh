0347fe000aafd0030391f0bf8e17fd2cd5ba85b3
Maurizio Pillitu <maoo@apache.org>
2015-10-27 19:44:08 +0100

    Update run-release.sh
    fixing nokogiri installation

--- run-release.sh
+++ run-release.sh
@@ -47,7 +47,8 @@ gem list > gems.list
 
 # Need this gem to create CHANGELOG.md
 # if grep -L github_changelog_generator gems.list; then
-  gem install github_changelog_generator
+PKG_CONFIG_PATH=/opt/chefdk/embedded/lib/pkgconfig gem install nokogiri
+gem install github_changelog_generator
 # fi
 
 function runTests () {
