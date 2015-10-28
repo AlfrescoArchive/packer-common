76158df14d814e1421271103cde395d840e928de
Maurizio Pillitu <maoo@apache.org>
2015-10-28 10:09:27 +0100

    Update run-test.sh
    Consolidated testing strategy

--- run-test.sh
+++ run-test.sh
@@ -3,11 +3,17 @@
 # Locate gem executables
 export PATH=/usr/local/packer:/opt/apache-maven/bin:/Users/Shared/apache-maven/3.2.3/bin:$HOME/.chefdk/gem/ruby/2.1.0/bin:/opt/chefdk/bin:/opt/chefdk/embedded/bin:$PATH
 
-# Fetching Gemfile, containing all gems used below for linting and testing
-curl -L https://raw.githubusercontent.com/Alfresco/packer-common/master/chef/Gemfile --no-sessionid > Gemfile
+# To avoid nasty nokogiri failures - https://github.com/chef/chef-dk/issues/278
+export PKG_CONFIG_PATH=/opt/chefdk/embedded/lib/pkgconfig
 
-# Installing gems in the user $HOME
-bundle update --path $HOME/.bundle
+export GEM_HOME=$HOME/.gemhome
+
+# Fetching Gemfile, containing all gems used below for linting and testing
+curl -L https://raw.githubusercontent.com/Alfresco/packer-common/master/chef/Gemfile --no-sessionid > GemfileTest
+
+# Installing gems in GEM_HOME
+bundle install --gemfile=GemfileTest
+rm -rf GemfileTest GemfileTest.lock
 
 # Running All checks per types
 find . -name "*.erb" -exec rails-erb-check {} \;
